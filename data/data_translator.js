6845a31baf7e31c84cfb9af1930ad2af1109d207
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-01-31 11:19:45 -0300

    refactoring done by openAi

--- data/data_translator.js
+++ data/data_translator.js
@@ -1,23 +1,18 @@
 //npm install xlsx
 //npm i --save https://cdn.sheetjs.com/xlsx-0.19.2/xlsx-0.19.2.tgz
 import returnInvalidDates from 'src/data_handler.js';
+
+const invalidDates = returnInvalidDates();
+
 const XLSX = require("xlsx");
+
 const invalidDates = returnInvalidDates();
 
 const workbook = XLSX.readFile("./data/Trocas_Bateria_RAs_GAs.xlsx");
-
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
-const XSLjsonArray = [];
+const options = {
+  header: 1
+};
 
-XSLjsonArray =  XLSX.utils.sheet_to_json(worksheet);
-
-const jsonExample = { //todos setados para string inicialmente mas sua versão final está explicita dentro do valor
-  sequencial: 'Number', 
-  numero_equipamento: 'string',
-  sigla_SE: 'string',
-  tipo_equip: 'string',
-  operacional: 'string',
-  data_ultima_troca: 'Date',
-  data_proxima_troca: 'Date'
-}
+const XSLjsonArray = XLSX.utils.sheet_to_json(worksheet, options);
