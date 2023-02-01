23fbe029e6ec9924df0c106a7340281016d464c8
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-02-01 07:45:41 -0300

    needed imports made

---
+++ index.js
@@ -0,0 +1,4 @@
+import {returnInvalidDates, arrayWithSettedDates} from './src/data_handler.js';
+import {dueNextArray, changeExpiredArray} from './src/functions.js';
+import XSLjsonArray from './data/data_translator.js';
+import returnInvalidDates from 'src/data_handler.js';
