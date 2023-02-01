bc0c06a4de8f504473badc7fff3cf0e299d318e5
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-02-01 08:04:48 -0300

    index structure built

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ import {returnInvalidDates, arrayWithSettedDates} from './src/data_handler.js';
 import {dueNextArray, changeExpiredArray} from './src/functions.js';
 import XSLjsonArray from './data/data_translator.js';
 import returnInvalidDates from 'src/data_handler.js';
+
+const handledDataJsonArray = arrayWithSettedDates(XSLjsonArray);
+
+const invalidDates = returnInvalidDates();
+
+const equipsChangeDueNext = dueNextArray(handledDataJsonArray);
+
+const equipsChangeExpired = changeExpiredArray(handledDataJsonArray);
+
+//test + debug
+//set up automated mail
+//send arrays 
+
+
