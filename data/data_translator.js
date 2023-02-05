a39b5c47fc91ad08cf8041a311c5e7e4021147ec
rdias66 <dias.a@live.com>
2023-02-05 14:39:13 -0300

    data translator debugged

--- data/data_translator.js
+++ data/data_translator.js
@@ -1,11 +1,7 @@
 //npm install xlsx
 //npm i --save https://cdn.sheetjs.com/xlsx-0.19.2/xlsx-0.19.2.tgz
 import XLSX from 'xlsx';
-const workbook = XLSX.readFile("./data/Trocas_Bateria_RAs_GAs.xlsx");
+const workbook = XLSX.readFile("data/test_sheet.xlsx");
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-const options = {
-  header: 1
-};
-const XLSXjsonArray = XLSX.utils.sheet_to_json(worksheet, options);
-
+const XLSXjsonArray = XLSX.utils.sheet_to_json(worksheet);
 export default XLSXjsonArray;
