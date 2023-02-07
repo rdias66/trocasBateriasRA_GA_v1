043c5992e2a23722c0e29cfb9938c078d43eaf52
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-02-07 09:19:09 -0300

    data translator verification method added

--- data/data_translator.js
+++ data/data_translator.js
@@ -4,8 +4,16 @@ const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 const dataArray = XLSX.utils.sheet_to_json(worksheet);
 
 const XLSXjsonArray = dataArray.map(equipJson => {
+         if(isEmpty(equipJson.data_ultima_troca)){
          equipJson.data_ultima_troca = new Date(equipJson.data_ultima_troca.replaceAll("/","-"));
          return equipJson;
+         }
+         
   });
+
+const isEmpty = key => {
+         const isEmpty = key !== null ? true : false;
+         return isEmpty;
+}
 
 export default XLSXjsonArray;
