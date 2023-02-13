38a2a9570bbf7840abd4c683ab9a402ca9358bcb
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-02-13 09:01:45 -0300

    import made

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import arrayWithSettedDates from './service/data_handler.js';//needs testing + debugging  logic working
 import {dueNextArray, changeExpiredArray} from './service/functions.js'; //needs testing + debugging + logic working
-import {XLSXjsonArray, invalidDatesArray} from './data/data_translator.js'; //working
+import {XLSXjsonArray, invalidDatesArray, createXLSXfileFrom} from './data/data_translator.js'; //working
 import sendEquips from './service/email_sender.js';//needs testing + debugging
 
 
