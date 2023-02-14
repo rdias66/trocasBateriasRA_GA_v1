e381c94557e6be193472488ccad05c10d160866a
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-02-14 12:17:40 -0300

    cleaned for addition on new module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import arrayWithSettedDates from './service/data_handler.js';//needs testing + debugging  logic working
 import {dueNextArray, changeExpiredArray} from './service/functions.js'; //needs testing + debugging + logic working
-import {XLSXjsonArray, invalidDatesArray, createXLSXfileFrom} from './data/data_translator.js'; //working
+import XLSXjsonArray from './data/data_translator.js'; //working
 import sendEquips from './service/email_sender.js';//needs testing + debugging
 
 
@@ -9,6 +9,7 @@ const handledDataJsonArray = arrayWithSettedDates(XLSXjsonArray);
 const equipsDueNext = dueNextArray(handledDataJsonArray);
 
 const equipsExpired = changeExpiredArray(handledDataJsonArray);
+
 
 //sendEquips(equipsChangeDueNext);
 //text not formated on email, only sendind jsons
