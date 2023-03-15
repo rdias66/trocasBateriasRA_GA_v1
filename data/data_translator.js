f9c40719d2b2a28a2283e818e62b664857bc9c68
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-03-15 09:51:25 -0300

    Update data_translator.js

--- data/data_translator.js
+++ data/data_translator.js
@@ -5,10 +5,10 @@ const workbook = XLSX.readFile("data/db_equips.xlsx");
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 const dataArray = XLSX.utils.sheet_to_json(worksheet);
 const invalidDatesArray = [];
-const savePath = './generatedSheets/'; // file path to be created on data section
+const savePath = 'data/'; // file path to be created on data section
 
 const XLSXjsonArray = dataArray.filter(equipJson => { //rebuilds the array, transforming the string dates into Date types, pushing invalid data into invalid dates array.
-         if(equipJson.data_ultima_troca != undefined){
+         if(equipJson.data_ultima_troca !== undefined){
          equipJson.data_ultima_troca = new Date(fixDateFormat(equipJson.data_ultima_troca)); //function built for an especific sheet
          return equipJson;
          }
@@ -21,4 +21,12 @@ const XLSXjsonArray = dataArray.filter(equipJson => { //rebuilds the array, tran
         return localStringArraySeparation[1] + "-" + localStringArraySeparation[0] + "-" + localStringArraySeparation[2];
 }
 
-export {XLSXjsonArray , invalidDatesArray};
+const newXLSXfile = (jsonArray, sheetName) => { 
+         const workbook = xlsx.utils.book_new();
+         const worksheet = xlsx.utils.json_to_sheet(jsonArray);
+         xlsx.utils.book_append_sheet(workbook, worksheet, sheetName);            
+         const buffer = xlsx.write(workbook, { type: 'buffer', bookType: 'xlsx' })
+         fs.writeFileSync(savePath + sheetName, buffer); //
+}
+
+export {XLSXjsonArray , invalidDatesArray, newXLSXfile};
